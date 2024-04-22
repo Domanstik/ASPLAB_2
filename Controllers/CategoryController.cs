d24c4166e0142d733c461092239b2d5b3ee9678f
Domanstik <87134258+Domanstik@users.noreply.github.com>
2024-04-22 19:47:40 +0300

    Add full category model

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPLAB_2.Data;
+using ASPLAB_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ASPLAB_2.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly CategoryContext dbCategoryContext;
+
+        public CategoryController(CategoryContext dbCategoryContext)
+        {
+            this.dbCategoryContext = dbCategoryContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await dbCategoryContext.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Category category_)
+        {
+            dbCategoryContext.AddAsync(category_);
+            dbCategoryContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Category");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var category = await dbCategoryContext.Categories.FindAsync(Id);
+            return View(category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category_)
+        {
+            var category = await dbCategoryContext.Categories.FindAsync(category_.Id);
+
+            if (category is not null)
+            {
+                category.Id = category_.Id;
+                category.Name = category_.Name;
+                category.Description = category_.Description;
+
+                await dbCategoryContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Category");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Category category_) 
+        {
+            var category = await dbCategoryContext.Categories.FindAsync(category_.Id);
+
+            if (category is not null)
+            {
+                dbCategoryContext.Categories.Remove(category);
+
+                await dbCategoryContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Category");
         }
     }
 }
